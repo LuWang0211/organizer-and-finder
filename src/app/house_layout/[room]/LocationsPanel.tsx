23c59d18907f0cfca8401d8918d0142b6577c77d
crystal <wangluuw@126.com>
2025-09-04 13:55:30 -0700

    fix

--- src/app/house_layout/[room]/LocationsPanel.tsx
+++ src/app/house_layout/[room]/LocationsPanel.tsx
@@ -1,10 +1,10 @@
 import { fetchLocationsByRoom } from "@/services/locationService";
-import LocationList from "./LocationsList";
+import LocationsList from "./LocationsList";
 
 export default async function LocationsPanel({ roomId }: { roomId: string }) {
   const locationsData = await fetchLocationsByRoom(roomId);
   return (
-    <LocationList locations={locationsData} roomId={roomId} loading={false} />
+    <LocationsList locations={locationsData} roomId={roomId} loading={false} />
   );
 }
 
