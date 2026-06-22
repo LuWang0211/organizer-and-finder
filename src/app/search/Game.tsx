7ed3a3a105919ee70b70a7f612336435c33f6511
Anchangsi <anchangsi@gmail.com>
2026-06-21 22:12:55 -0700

    Refactor the Search page, so it uses the React version SearchBar component.

--- src/app/search/Game.tsx
+++ src/app/search/Game.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import PhaserGame from "@phaser/Game";
-import { useCallback } from "react";
+import PhaserGame, { type PhaserGameRef } from "@phaser/Game";
+import { type FormEvent, useCallback, useId, useRef } from "react";
 import { useMeasure } from "react-use";
 import type { ItemType } from "@/services/itemService";
+import { SearchBar } from "@/ui/components/SearchBar";
 import { MarqueeTooltipOverlay } from "./components/MarqueeTooltipOverlay";
-import { UIScene } from "./scenes/UIScene";
+import { EVENT_LETTER_FALL, UIScene } from "./scenes/UIScene";
 
 type SearchGameProps = {
   initialItems: ItemType[];
@@ -24,10 +25,29 @@ export default function SearchGame({ initialItems }: SearchGameProps) {
     [initialItems],
   );
 
+  const gameRef = useRef<PhaserGameRef>(null);
+  const searchId = useId();
+
+  const onSearchTextChange = (event: FormEvent<HTMLInputElement>) => {
+    const text = (event.target as HTMLInputElement).value;
+    gameRef.current?.emit(EVENT_LETTER_FALL, text);
+  };
+
   return (
     <div className="relative w-full h-full overflow-x-hidden">
       <div ref={containerMeasure} className="absolute inset-0" />
-      <PhaserGame secondScene={UIScene} onGameCreated={onGameCreated} />
+      <SearchBar
+        inputId={searchId}
+        placeholder="hello wawa"
+        onSearch={(value) => console.log("Search query:", value)}
+        onChange={onSearchTextChange}
+        className="absolute top-14 left-1/2 -translate-x-1/2 z-10"
+      />
+      <PhaserGame
+        secondScene={UIScene}
+        onGameCreated={onGameCreated}
+        ref={gameRef}
+      />
       <MarqueeTooltipOverlay zoom={zoom} />
     </div>
   );
